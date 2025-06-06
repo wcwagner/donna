af81893719bb8b330385bcbb23341a41fb16a047
William Wagner <wcwwagner@gmail.com>
2025-06-05 21:53:03 -0400

    Scaffold iOS app with SwiftData/CloudKit

---
+++ donnaTests/donnaTests.swift
@@ -0,0 +1,17 @@
+//
+//  donnaTests.swift
+//  donnaTests
+//
+//  Created by William Wagner on 6/5/25.
+//
+
+import Testing
+@testable import donna
+
+struct donnaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ donnaUITests/donnaUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  donnaUITestsLaunchTests.swift
+//  donnaUITests
+//
+//  Created by William Wagner on 6/5/25.
+//
+
+import XCTest
+
+final class donnaUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
